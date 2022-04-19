7626f6ba7e98ae3e7f0870e273415884531c0986
Laurent <laurent@toussaint.io>
2022-04-19 18:24:24 +0200

    feat(Dust): create component Dust

---
+++ src/components/Dust.tsx
@@ -0,0 +1,46 @@
+import { useMemo } from "react";
+import cn from "classnames";
+import BN from "bn.js";
+
+export interface DustProps {
+  boxSize: number;
+  x: BN;
+  y: BN;
+  size: "tiny" | "regular" | "big";
+}
+
+export default function Dust({ boxSize, x, y, size }: DustProps) {
+  const containerStyle = useMemo(() => {
+    return {
+      left: (x.toNumber() + 0.5) * boxSize,
+      top: (y.toNumber() + 0.5) * boxSize,
+    };
+  }, [boxSize, x, y]);
+
+  const dustClassName = useMemo(() => {
+    switch (size) {
+      case "tiny":
+        return "w-[3px] h-[3px]";
+      case "big":
+        return "w-[5px] h-[5px]";
+      case "regular":
+      default:
+        return "w-[4px] h-[4px]";
+    }
+  }, [size]);
+
+  const filterAnimation = useMemo(() => {
+    return `animate-flicker${Math.ceil(Math.random() * 3 + 1)} animate-dust-color`;
+  }, []);
+
+  return (
+    <div
+      style={containerStyle}
+      className={cn(
+        "absolute flex items-center justify-center rounded bg-light-blue transition-all duration-700 ease-in-out",
+        filterAnimation,
+        dustClassName
+      )}
+    ></div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,66 @@
 const colors = require("tailwindcss/colors");
 
+const customColor = {
+  "space-dark-blue": "#0D0814",
+  "space-blue": "#0E0D2E",
+  "space-blue-disabled": "#AABCD2",
+  "mid-blue": "#0009BC",
+  "light-blue": "#00FFE4",
+  "off-white": "#E3DAD4",
+  "light-purple": "#EBDDFF",
+  orange: "#FF9000",
+  "space-purple": "#AE00FF",
+  black: "#000000",
+  nickel: "#666666",
+  snow: "#F4F4F4",
+};
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  safelist: ["animate-flicker2", "animate-flicker3", "animate-flicker4", "animate-dust-color"],
   theme: {
     extend: {
       colors: {
         primary: colors.indigo,
         secondary: colors.slate,
+        ...customColor,
       },
+      animation: {
+        flicker2: "flicker 2s linear infinite",
+        flicker3: "flicker 3s linear infinite",
+        flicker4: "flicker 4s linear infinite",
+        "dust-color": "dust-color 4s linear infinite",
+      },
+      keyframes: {
+        flicker: {
+          "10%": { opacity: 1 },
+          "35%": { opacity: 0.8 },
+          "60%": { opacity: 0.7 },
+          "75%": { opacity: 0.7 },
+          "90%": { opacity: 1 },
+        },
+        "dust-color": {
+          "0%": {
+            "background-color": "#00ffe4",
+            "box-shadow": "0px 0px 2px 2px #00ffe4, 0px 0px 7px 4px #00ffe4, 0px 0px 20px 5px #00ffe4",
+          },
+          "33%": {
+            "background-color": "#ff9000",
+            "box-shadow": "0px 0px 2px 2px #ff9000, 0px 0px 7px 4px #ff9000, 0px 0px 20px 5px #ff9000",
+          },
+          "67%": {
+            "background-color": "#ae00ff",
+            "box-shadow": "0px 0px 2px 2px #ae00ff, 0px 0px 7px 4px #ae00ff, 0px 0px 20px 5px #ae00ff",
+          },
+          "100%": {
+            "background-color": "#00ffe4",
+            "box-shadow": "0px 0px 2px 2px #00ffe4, 0px 0px 7px 4px #00ffe4, 0px 0px 20px 5px #00ffe4",
+          },
+        },
+      },
+    },
+    boxShadow: {
+      dust: "0px 0px 2px 2px #08f9ff, 0px 0px 7px 4px #08f9ff, 0px 0px 20px 5px #08f9ff",
     },
   },
   plugins: [],
