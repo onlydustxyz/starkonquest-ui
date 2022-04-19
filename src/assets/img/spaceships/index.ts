d73f4023149897ff08eabfbf849ace8599a33265
Laurent <laurent@toussaint.io>
2022-04-19 18:32:39 +0200

    feat(Ship): create component Ship

---
+++ src/assets/img/spaceships/index.ts
@@ -0,0 +1,9 @@
+import ship1 from "./spaceship-1.png";
+import ship2 from "./spaceship-2.png";
+
+const ships: Record<number, string> = {
+  1: ship1,
+  2: ship2,
+};
+
+export default ships;
---
+++ src/components/Ship.tsx
@@ -0,0 +1,65 @@
+import { useEffect, useMemo, useState } from "react";
+import { usePrevious } from "react-use";
+import cn from "classnames";
+import BN from "bn.js";
+
+import ships from "src/assets/img/spaceships";
+
+import styles from "./Ship.module.css";
+
+export interface ShipProps {
+  boxSize: number;
+  shipData: {
+    shipId: string;
+    position: {
+      x: BN;
+      y: BN;
+    };
+    shipIndex: number;
+  };
+}
+
+export default function Ship({ boxSize, shipData }: ShipProps) {
+  const previousPosition = usePrevious({ x: shipData.position.x, y: shipData.position.y });
+  const [angle, setAngle] = useState(0);
+
+  const shipImage = useMemo(() => {
+    return ships[shipData.shipIndex] || ships[1];
+  }, [shipData.shipIndex]);
+
+  useEffect(() => {
+    const prevX = previousPosition?.x.toNumber() || 0;
+    const prevY = previousPosition?.y.toNumber() || 0;
+
+    const newX = shipData.position.x.toNumber();
+    const newY = shipData.position.y.toNumber();
+
+    if (newX === prevX && newY === prevY) {
+      return;
+    }
+
+    const deltaX = newX - prevX;
+    const deltaY = newY - prevY;
+
+    const rad = Math.atan2(deltaY, deltaX);
+
+    setAngle(rad * (180 / Math.PI));
+  }, [shipData.position.x, shipData.position.y, previousPosition?.x, previousPosition?.y]);
+
+  const containerStyle = useMemo(() => {
+    return {
+      transform: `rotate(${angle}deg)`,
+      left: (shipData.position.x.toNumber() + 0.5) * boxSize,
+      top: (shipData.position.y.toNumber() + 0.5) * boxSize,
+    };
+  }, [angle, boxSize, shipData.position.x, shipData.position.y]);
+
+  return (
+    <div
+      style={containerStyle}
+      className={cn("absolute flex items-center justify-center transition-all duration-700 ease-in-out", styles.ship)}
+    >
+      <img src={shipImage} width={`${boxSize}px`} height={`${boxSize}px`} />
+    </div>
+  );
+}
