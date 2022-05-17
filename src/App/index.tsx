e2e55bcee2fc17315ad8edd4b4a65ed462a4ab1a
Laurent <laurent@toussaint.io>
2022-05-17 17:14:06 +0200

    refactor(App): export routes in a dedicated component

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,13 +1,9 @@
 import { InjectedConnector, StarknetProvider } from "@starknet-react/core";
-import { lazy, Suspense } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Provider } from "starknet";
 
 import config from "src/config";
 
-const GameScreen = lazy(() => import("src/screens/Game"));
-const TournamentScreen = lazy(() => import("src/screens/Tournament"));
-const RegisterShipScreen = lazy(() => import("src/screens/RegisterShip"));
+import AppRoutes from "./Routes";
 
 export default function App() {
   const connectors = [new InjectedConnector()];
@@ -15,34 +11,7 @@ export default function App() {
 
   return (
     <StarknetProvider connectors={connectors} defaultProvider={defaultProvider} autoConnect>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/game/:transactionHash"
-            element={
-              <Suspense fallback={<>...</>}>
-                <GameScreen />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/tournament/:tournamentAddress"
-            element={
-              <Suspense fallback={<>...</>}>
-                <TournamentScreen />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/tournament/:tournamentAddress/register"
-            element={
-              <Suspense fallback={<>...</>}>
-                <RegisterShipScreen />
-              </Suspense>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
+      <AppRoutes />
     </StarknetProvider>
   );
 }
