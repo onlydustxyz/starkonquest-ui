5dc2b2909960dd3fb9eda1c61984ea74450c2252
Laurent <laurent@toussaint.io>
2022-05-20 15:43:25 +0200

    refactor: remove unused code

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,11 +1,6 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly STARKONQUEST_RAND_ADDRESS: string;
-  readonly STARKONQUEST_BATTLE_ADDRESS: string;
-  readonly STARKONQUEST_DUST_ADDRESS: string;
-  readonly STARKONQUEST_SHIP_ADDRESS: string;
-
   readonly STARKONQUEST_PROVIDER_HOSTNAME: string;
 }
 
